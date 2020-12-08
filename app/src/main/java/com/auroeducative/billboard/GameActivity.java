807ef85fe1ff1ef66e62e8a61340323ea95c508f
hamzakhurshid12 <hkhurshid.bese16seecs@seecs.edu.pk>
2020-12-08 17:25:33 +0500

    Add lessons/tutorials Add switch to lesson screen on button press event Fix Game Scale

--- app/src/main/java/com/auroeducative/billboard/GameActivity.java
+++ app/src/main/java/com/auroeducative/billboard/GameActivity.java
@@ -6,20 +6,35 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.webkit.WebView;
 
 public class GameActivity extends Activity {
+
+    private String mode = "ad"; // ad, tutorial, game
+    WebView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        WebView view = findViewById(R.id.webView);
+        view = findViewById(R.id.webView);
         view.clearCache(true);
         view.clearHistory();
+        view.getSettings().setMediaPlaybackRequiresUserGesture(false);
         view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl("http://nwqm.pe.hu/AuroEducative/AuroEducative/count-js_game/count-game.html");
+        view.loadUrl("http://nwqm.pe.hu/AuroEducative/AdVideos/PlayAd.html");
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if(mode.equals("ad")){
+            view.loadUrl("http://nwqm.pe.hu/AuroEducative/AuroEducative/PlayTutorial.html");
+        } else {
+            super.dispatchKeyEvent(event);
+        }
+        return true;
     }
 }
